08592236efd7dd707bdc669565e70c0e5051d8b4
Sumit <sumit.wankhede@hotmail.com>
2020-07-14 16:38:38 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,2 +1,10 @@
-print("I love you Prachi")
-print("I love you too Gobu")
+print("Welcome to My calculator")
+
+print("Enter the 1st number")
+#input first number here
+
+print("Enter the 2nd number")
+#input second number here
+
+print("Here is the addition:")
+#print result here
