2025b6b35715fc7238725059fec3c00660f4866a
Sumit <sumit.wankhede@hotmail.com>
2020-07-14 16:56:50 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 print("Welcome to My calculator")
 
-print("Enter the 1st number")
+#print("Enter the 1st number")
 #input first number here
-
-print("Enter the 2nd number")
+num1 = input("Enter the 1st number: ")
+#print("Enter the 2nd number")
 #input second number here
-
+num2 = input("Enter the 2nd number: ")
 print("Here is the addition:")
 #print result here
+sum = int(num1) + int(num2)
+print(sum)
